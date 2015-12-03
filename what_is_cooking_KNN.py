aca2590d95d0e51313a863f4e0dc0bcfb87d1ec1
Gakaza <gakaza3333@gmail.com>
2015-12-03 17:16:00 +0800

    implementation of the knn with randomized search

--- what_is_cooking_KNN.py
+++ what_is_cooking_KNN.py
@@ -2,27 +2,44 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.cross_validation import KFold, train_test_split
 from sklearn.metrics import confusion_matrix, mean_squared_error
-from sklearn.grid_search import GridSearchCV
+from sklearn.grid_search import GridSearchCV, RandomizedSearchCV
 import pandas as pd
 import numpy as np
 import xgboost as xgb
 import re
 import csv
 
-
-#Soft voting
 #from sklearn import datasets #for the iris example
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
 #from itertools import product
+from sklearn import preprocessing
+from operator import itemgetter
+from sklearn import cross_validation
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.stats import randint as sp_randint
 #from sklearn.ensemble import VotingClassifier
+from time import time
+#import pickle
+#from math import sqrt
+
+# Utility function to report best scores
+def report(grid_scores, n_top=3):
+    top_scores = sorted(grid_scores, key=itemgetter(1), reverse=True)[:n_top]
+    for i, score in enumerate(top_scores):
+        print("Model with rank: {0}".format(i + 1))
+        print("Mean validation score: {0:.3f} (std: {1:.3f})".format(
+              score.mean_validation_score,
+              np.std(score.cv_validation_scores)))
+        print("Parameters: {0}".format(score.parameters))
+        print("")
+
+
+
 
 print 'started'
 # read train and test data from json
 traindf = pd.read_json("train.json")
 testdf = pd.read_json("test.json")
-
 
 # data preprocessing
 # 1. replace '-' with '_'
@@ -36,122 +53,95 @@ traindf['processed_ingredients_string'] = [','.join(z).strip() for z in traindf[
 testdf['processed_ingredients_string'] = [','.join(z).strip() for z in testdf['processed_ingredients']]
 
 
-# Document term matrix
-vectorizer = CountVectorizer()
-vectorizer.fit_transform(traindf['processed_ingredients_string'])
-# print vectorizer.get_feature_names()
-strain = vectorizer.transform(traindf['processed_ingredients_string']) #our input in numbers
-# print strain.todense().shape
-stest = vectorizer.transform(testdf['processed_ingredients_string']) #our testing data in numbers
-# print stest.todense().shape
-
-
+'''
 # Map cuisine to integer(start from 0)
 item = set()
 for i in traindf['cuisine']:
     item.add(i)
 item = sorted(item)
-
 cuisine_array = np.array(item)
 cuisine_dict = dict(zip(cuisine_array, range(0, len(cuisine_array))))
-
 traindf['cuisine_number'] = [cuisine_dict[n] for n in traindf['cuisine']]
 cuisine_label = traindf['cuisine_number']   #our output in number
+'''
+
+# Encode cuisine to integer(start from 0)
+le = preprocessing.LabelEncoder()
+le.fit(traindf['cuisine'])
+traindf['cuisine_number'] = le.transform(traindf['cuisine'])
+cuisine_label = traindf['cuisine_number']
+
+# Document term matrix
+vectorizer = CountVectorizer()
+vectorizer.fit_transform(traindf['processed_ingredients_string'])
+dtm_train = vectorizer.transform(traindf['processed_ingredients_string']) #our input in numbers
+dtm_test = vectorizer.transform(testdf['processed_ingredients_string']) #our testing data in numbers
+
+# Tf–idf term
+tfidf_trans = TfidfTransformer()
+tfidf_train = tfidf_trans.fit_transform(dtm_train)
+tfidf_test = tfidf_trans.fit_transform(dtm_test)
+
 
 #soft voting code
 # Loading some example data
-#iris = datasets.load_iris() #example data set
-X = strain
-y = cuisine_label
 
-#K-nearest neighbours (KNN) classification
-#knn = KNeighborsClassifier(n_neighbors=5)
-#knn.fit(X,y)
-#predicted = knn.predict(stest)
+#Cross-validation test code; need to do random search first
+'''
+#from sklearn import neighbors
 
-from sklearn.cross_validation import train_test_split
-from sklearn import cross_validation
-from sklearn import neighbors
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.4, random_state=0)
+#X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.4, random_state=0)
+#knn = neighbors.KNeighborsClassifier(n_neighbors=20, leaf_size=100)
 
-knn = neighbors.KNeighborsClassifier(n_neighbors=1)
-knn.fit(X_train, y_train)
-predicted = knn.predict(X_test)
+#these two lines will consume too much memory
+#knn.fit(dtm_train, cuisine_label) 
+#knn.score(dtm_train, cuisine_label)
 
+#knn.fit(X_train, y_train)
+#knn.score(X_train, y_train)
+#predicted = knn.predict(X_test)
 
 from sklearn import metrics
 print metrics.classification_report(y_test, predicted)
 print metrics.confusion_matrix(y_test, predicted)
-print metrics.f1_score(y_test, predicted)
+print metrics.f1_score(y_test, predicted)   #could change another algorithm
 
 from sklearn.cross_validation import cross_val_score
 
-scores = cross_val_score(knn, X, y, cv=5)
+scores = cross_val_score(knn, X, y, cv=5) #need to change?
 print scores
-# Ensemble Training classifiers test code
-'''
-clf1 = DecisionTreeClassifier(max_depth=4)
-clf2 = KNeighborsClassifier(n_neighbors=7)
-clf3 = SVC(kernel='rbf', probability=True)
-eclf = VotingClassifier(estimators=[('dt', clf1), ('knn', clf2), ('svc', clf3)], voting='soft', weights=[2,1,2])
-
-
-clf1 = clf1.fit(X,y)
-clf2 = clf2.fit(X,y)
-clf3 = clf3.fit(X,y)
-eclf = eclf.fit(X,y)
 '''
 
+'''-------------------------------------------------------------'''
+'''sampele randome forest with grid search and randomized search'''
+'''-------------------------------------------------------------'''
 
-# xgboost need its own data structure Dmatrix
-'''dtrain = xgb.DMatrix(strain, label = cuisine_label, missing = 0)
-dtest = xgb.DMatrix(stest)
+from sklearn.neighbors import KNeighborsClassifier
+clf = KNeighborsClassifier()
+# specify parameters and distributions to sample from
+param_dist = {"n_neighbors": [10, 30, 50, 70],
+              "weights": ["uniform", "distance"],
+              "algorithm": ["auto", "ball_tree", "kd_tree", "brute"],
+              "leaf_size": sp_randint(50, 300)}
 
+# run randomized search
+n_iter_search = 30
+random_search = RandomizedSearchCV(clf, param_distributions=param_dist, n_iter=n_iter_search)
 
-# set xgboost parameter
-param = {}
-# use softmax multi-class classification
-param['objective'] = 'multi:softmax'
-# scale weight of positive examples
-param['eta'] = 0.05
-param['max_depth'] = 10
-param['num_class'] = 20
-num_round = 100
-# watchlist = [ (xg_train,'train'), (xg_test, 'test') ]
+# DTM
+start = time()
 
+random_search.fit(dtm_train, cuisine_label)
 
-# start prediction
-bst = xgb.train(param, dtrain, num_round)
-pred = bst.predict(dtest)
-#xgb.plot_importance(bst)
+print("RandomizedSearchCV took %.2f seconds for %d candidates"
+      " parameter settings." % ((time() - start), n_iter_search))
+report(random_search.grid_scores_)
 
+# TF-IDF
+start = time()
+random_search.fit(tfidf_train, cuisine_label)
+print("RandomizedSearchCV took %.2f seconds for %d candidates"
+      " parameter settings." % ((time() - start), n_iter_search))
+report(random_search.grid_scores_)
 
-xgb.cv(param, dtrain, num_round, nfold=5,
-       metrics={'merror'}, seed = 0)
-
-# rng = np.random.RandomState(31337)
-# kf = KFold(traindf.shape[0], n_folds=2, shuffle=True, random_state=rng)
-# for train_index, test_index in kf:
-#     xgb_model = xgb.XGBClassifier().fit(strain.todense()[train_index], traindf['cuisine_number'][train_index])
-#     predictions = xgb_model.predict(strain.todense()[test_index])
-#     actuals = traindf['cuisine_number'][test_index]
-#     print(confusion_matrix(actuals, predictions))
-
-
-testdf['cuisine'] = [cuisine_array[int(n)] for n in pred]
-predict_dict = dict(zip(testdf['id'], testdf['cuisine']) )
-with open('predict_result.csv', 'w') as csvfile:
-    fieldnames = ['id', 'cuisine']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-    writer.writeheader()
-    for key, value in predict_dict.iteritems():
-        writer.writerow({'id': key, 'cuisine': value})
-<<<<<<< Updated upstream
-
-
-=======
-        
-'''
->>>>>>> Stashed changes
 print 'finished'
