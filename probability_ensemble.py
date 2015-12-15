bb479f1da2aa494e36703d1f37cc65145073c483
unknown <xxzero12@gmail.com>
2015-12-15 14:35:43 +0800

    use full classifier

--- probability_ensemble.py
+++ probability_ensemble.py
@@ -60,21 +60,20 @@ dtm_test = vectorizer.transform(testdf['processed_ingredients_string']).toarray(
 
 
 #%%
-# tf-idf transforming
-# tfidf_trans = TfidfTransformer()
-# tfidf_train = tfidf_trans.fit_transform(dtm_train).toarray()
-# tfidf_test = tfidf_trans.transform(dtm_test).toarray()
+#tf-idf transforming
+tfidf_trans = TfidfTransformer()
+tfidf_train = tfidf_trans.fit_transform(dtm_train).toarray()
+tfidf_test = tfidf_trans.transform(dtm_test).toarray()
 
 #%%
 clf1 = RandomForestClassifier(bootstrap=False, min_samples_leaf=1, n_estimators=275, min_samples_split=3, criterion='gini', max_features=43, max_depth=None)
-# clf2 = RandomForestClassifier(bootstrap=False, min_samples_leaf=1, n_estimators=150, min_samples_split=6, criterion='gini', max_features=40, max_depth=None)
+clf2 = RandomForestClassifier(bootstrap=False, min_samples_leaf=1, n_estimators=150, min_samples_split=6, criterion='gini', max_features=40, max_depth=None)
 clf3 = ExtraTreesClassifier(max_features=6, n_estimators=100, criterion='gini', max_depth=None)
-# clf4 = ExtraTreesClassifier(max_features=1, n_estimators=70, criterion='entropy', max_depth=None)
-#clf5 = LinearSVC(penalty='l2', C=0.2, tol=0.0001, dual=False)
-clf5 = SGDClassifier(penalty='l2', loss='modified_huber')
-# clf6 = LinearSVC(penalty='l2', C=0.6, tol=0.01, dual=False)
+clf4 = ExtraTreesClassifier(max_features=1, n_estimators=70, criterion='entropy', max_depth=None)
+clf5 = SGDClassifier(penalty='l2', loss='log')
+clf6 = SGDClassifier(penalty='l2', loss='modified_huber')
 clf7 = LogisticRegression(penalty='l2', C=1, tol=0.0001, dual=False)
-# clf8 = LogisticRegression(penalty='l2', C=8, tol=0.01, dual=False)
+clf8 = LogisticRegression(penalty='l2', C=8, tol=0.01, dual=False)
 
 num_rounds = 2000
 param = {
@@ -84,7 +83,7 @@ param = {
    'num_class':20
 }
 clf9 = xgb.train(param, xgb.DMatrix(dtm_train, cuisine_label), num_rounds)
-# clf10 = xgb.train(param, xgb.DMatrix(tfidf_train, cuisine_label), num_rounds)
+clf10 = xgb.train(param, xgb.DMatrix(tfidf_train, cuisine_label), num_rounds)
 
 
 #%%
@@ -92,35 +91,35 @@ clf9 = xgb.train(param, xgb.DMatrix(dtm_train, cuisine_label), num_rounds)
 clf1.fit(dtm_train, cuisine_label)
 test_pred1 = clf1.predict_proba(dtm_test).reshape(9944, 20)
 #rf on tfidf
-# clf2.fit(tfidf_train, cuisine_label)
-# test_pred2 = clf2.predict_proba(tfidf_test).reshape(9944, 20)
+clf2.fit(tfidf_train, cuisine_label)
+test_pred2 = clf2.predict_proba(tfidf_test).reshape(9944, 20)
 #ex on dtm
 clf3.fit(dtm_train, cuisine_label)
 test_pred3 = clf3.predict_proba(dtm_test).reshape(9944, 20)
 #ex on tfidf
-# clf4.fit(tfidf_train, cuisine_label)
-# test_pred4 = clf4.predict_proba(tfidf_test).reshape(9944, 20)
+clf4.fit(tfidf_train, cuisine_label)
+test_pred4 = clf4.predict_proba(tfidf_test).reshape(9944, 20)
 #%%
 #lsvc on dtm
 clf5.fit(dtm_train, cuisine_label)
 test_pred5 = clf5.predict_proba(dtm_test).reshape(9944, 20)
 #lsvc on tfidf
-# clf6.fit(tfidf_train, cuisine_label)
-# test_pred6 = clf6.predict_proba(tfidf_test).reshape(9944, 20)
+clf6.fit(tfidf_train, cuisine_label)
+test_pred6 = clf6.predict_proba(tfidf_test).reshape(9944, 20)
 #lr on dtm
 clf7.fit(dtm_train, cuisine_label)
 test_pred7 = clf7.predict_proba(dtm_test).reshape(9944, 20)
 #lr on tfidf
-# clf8.fit(tfidf_train, cuisine_label)
-# test_pred8 = clf8.predict_proba(tfidf_test).reshape(9944, 20)
+clf8.fit(tfidf_train, cuisine_label)
+test_pred8 = clf8.predict_proba(tfidf_test).reshape(9944, 20)
 #xgb on dtm
 test_pred9 = clf9.predict(xgb.DMatrix(dtm_test)).reshape(9944, 20)
 #xgb on tfidf
-# test_pred10 = clf10.predict(xgb.DMatrix(tfidf_test)).reshape(9944, 20)
+test_pred10 = clf10.predict(xgb.DMatrix(tfidf_test)).reshape(9944, 20)
 
 
 #%%
-blend_prob = test_pred1 + test_pred3 + test_pred5 + test_pred7 + test_pred9 
+blend_prob = test_pred1 + test_pred2 + test_pred3 + test_pred4 + test_pred5 + test_pred6 + test_pred7 + test_pred8 + test_pred9 + test_pred10 
 predict_result = np.argmax(blend_prob, axis=1) 
 #%%
 testdf['cuisine'] = le.inverse_transform(predict_result.astype(np.int32))
